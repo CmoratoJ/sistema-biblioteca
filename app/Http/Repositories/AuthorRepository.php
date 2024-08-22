b6483ed58df23081b18d372268077934c791be8b
Caio Morato <caiomorato5@gmail.com>
2024-08-22 08:11:11 -0300

    feat:Add CRUD Author and Swagger documentation in Author

---
+++ app/Http/Repositories/AuthorRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use App\Http\Repositories\Interface\IAuthorRepository;
+use App\Http\Requests\CreateOrUpdateAuthorRequest;
+use App\Models\Author;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+
+class AuthorRepository implements IAuthorRepository
+{
+
+    public function persist(CreateOrUpdateAuthorRequest $request, int $id = null): Author
+    {
+        Cache::forget('all_authors');
+
+        $author = $id ? Author::find($id) : new Author();
+        $author->fill($request->all());
+        $author->save();
+        return $author;
+    }
+
+    public function findById(int $id): Author
+    {
+        return Author::with('books')->find($id);
+    }
+
+    public function findAll(): Collection
+    {
+        return Cache::remember('all_authors', 3600, fn () => Author::with('books')->get());
+    }
+
+    public function delete(Author $author): void
+    {
+        Cache::forget('all_authors');
+        $author->delete();
+    }
+}
